eeac9b88452b2b0f6b6667a91672f171387ec398
phanhuutoan <73571447+phanhuutoan@users.noreply.github.com>
2021-04-23 17:35:32 +0700

    7m0ufm - dockerizing backend

--- scripts/docker-start-app.sh
+++ scripts/docker-start-app.sh
@@ -1,5 +1,13 @@
-npm run migrate:docker Init_app_docker
-npm run build
-npm run migrate:up
+#!/bin/bash
+# npm run migrate:docker Init_app_docker
+
+shopt -s nullglob dotglob     # To include hidden files
+files=(/home/app/src/migrations/*)
+echo ${#files[@]}
+if [ ${#files[@]} -gt 0 ]
+  then 
+    npm run build
+    npm run migrate:up
+  fi
 
 npm run start:dev
