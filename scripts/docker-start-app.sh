d052a4bcfb417063d3596fa37bc47dbe701086f7
phanhuutoan <73571447+phanhuutoan@users.noreply.github.com>
2021-04-23 17:56:20 +0700

    small bug

--- scripts/docker-start-app.sh
+++ scripts/docker-start-app.sh
@@ -1,13 +1,4 @@
-#!/bin/bash
-# npm run migrate:docker Init_app_docker
-
-shopt -s nullglob dotglob     # To include hidden files
-files=(/home/app/src/migrations/*)
-echo ${#files[@]}
-if [ ${#files[@]} -gt 0 ]
-  then 
-    npm run build
-    npm run migrate:up
-  fi
-
+npm run migrate:docker Init_app_docker
+npm run build
+npm run migrate:up
 npm run start:dev
