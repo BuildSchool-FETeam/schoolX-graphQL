3bd6b47c98d68f4e859ce94f4ccc18c07477a186
toan.phan <phanhuutoan.dev@outlook.com>
2021-07-23 22:31:39 +0700

    working with circle CI

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,3 +1,5 @@
+echo "$DOCKER_PASSOWRD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
 docker build -t superknife0512/schoolx-graphql -f Dockerfile . 
 # docker build -t superknife0512/schoolx-python-server -f ./servers/python/Dockerfile.dev ./servers/python 
 # docker build -t superknife0512/schoolx-java-server -f ./servers/java/Dockerfile.dev ./servers/java 
