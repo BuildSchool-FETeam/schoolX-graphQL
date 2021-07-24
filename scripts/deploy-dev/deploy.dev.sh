056439406e5e0868db79362dc9127728d62550bf
toan.phan <phanhuutoan.dev@outlook.com>
2021-07-24 21:47:06 +0700

    dev - test-deploy v12

--- scripts/deploy-dev/deploy.dev.sh
+++ scripts/deploy-dev/deploy.dev.sh
@@ -13,12 +13,12 @@ COMMAND="""
   git stash && 
   sudo git checkout dev &&
   sudo git pull origin dev &&
-  sudo docker-compose down &&
-  sudo docker pull superknife0512/schoolx-graphql &&
-  sudo docker pull superknife0512/schoolx-python-server &&
-  sudo docker pull superknife0512/schoolx-java-server &&
-  sudo docker pull superknife0512/schoolx-js-server &&
-  sudo docker-compose -f docker-compose.prod.yml up -d
+  docker-compose down &&
+  docker pull superknife0512/schoolx-graphql &&
+  docker pull superknife0512/schoolx-python-server &&
+  docker pull superknife0512/schoolx-java-server &&
+  docker pull superknife0512/schoolx-js-server &&
+  docker-compose -f docker-compose.prod.yml up -d
 """
 gcloud auth activate-service-account --key-file=/app/compute-service-account.json
 gcloud compute ssh --project=$PROJECT --zone=us-east1-b $INSTANCE --command="$COMMAND" -q
